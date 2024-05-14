8179e073dc447c9be4e28e05a56753b09bea9e2a
Mr.Robot != false <elisiomassaqui8@gmail.com>
2024-05-14 19:50:33 +0100

    duas formulas da spherical interpolation

--- numero.js
+++ numero.js
@@ -1,9 +1,21 @@
-num1 = 20;
-num2 = 30;
-num3 = 0.8;
+num1 = 2;
+num2 = 5;
+num3 = 0.5;
 
-function soma(a, b, t) {
+num5 = 5 * 0.5;
+
+function slerpOne(a, b, t) {
+
+    return (1- t) * a + t * b;
+
+}
+
+function slerp(a, b, t) {
     return a + (b - a) * t;
 }
 
-console.log("Spherical Interpolation" + soma(a, b, t));
+slerpOne = slerpOne(num1, num2, num3);
+slerp = slerp(num1, num2, num3)
+
+
+console.log(slerpOne + "\n" + slerp + "\n" + num5);
