80ce7b842445b72e0269ad81b10b172b0922d7b8
Mr.Robot != false <elisiomassaqui8@gmail.com>
2024-05-14 20:16:48 +0100

    mais ou menos uma calculadora

---
+++ calculadoraJS/calculadora.js
@@ -0,0 +1,9 @@
+var somar = require("./add.js");
+var div = require("./div.js");
+var multi = require("./multi.js");
+var sub = require("./sub.js");
+
+console.log(somar(3, 6));
+console.log(div(3, 6));
+console.log(multi(1, 6));
+console.log(sub(3, 6));
