d4aa1e188c14efb8a2cf758437266c047debff71
Mr.Robot <elisiomassaqui8@gmail.com>
2024-05-15 21:55:24 +0100

    rotas principal e secundarias com express

--- servidor/servidorExpress.js
+++ servidor/servidorExpress.js
@@ -4,6 +4,10 @@ const servidorExpress = express();
 
 servidorExpress.get("/", function (req, res) {
     res.end("Servidor express");
-}).listen(8081);
+}).listen(8081);;
+
+servidorExpress.get("/sobre", function (req, res) {
+    res.end("Esse servidor foi construido com framework principal de node pra backend");
+});
 
 console.log("servidorExpress aberto");
