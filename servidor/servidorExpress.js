70bd61896dfc02d1096a408403df0e39a57d7705
Mr.Robot <elisiomassaqui8@gmail.com>
2024-05-16 21:00:05 +0100

    Melhor que essa linha esteja sempre no final mesmo

--- servidor/servidorExpress.js
+++ servidor/servidorExpress.js
@@ -1,13 +1,17 @@
+//Pegar modulo express
 const express = require("express");
 
+//Instancia do servidor
 const servidorExpress = express();
 
+//Rotas
 servidorExpress.get("/", function (req, res) {
     res.end("Servidor express");
-}).listen(8081);;
+});
 
 servidorExpress.get("/sobre", function (req, res) {
     res.end("Esse servidor foi construido com framework principal de node pra backend");
 });
 
-console.log("servidorExpress aberto");
+//Melhor que essa linha esteja sempre no final mesmo.
+servidorExpress.listen(8081, function(){console.log("Express aberto com sucesso");});
