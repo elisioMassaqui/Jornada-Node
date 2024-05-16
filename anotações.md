bbb51e407cad8a6d3dcd64de4cc5270cb78828f4
Mr.Robot <elisiomassaqui8@gmail.com>
2024-05-16 21:40:17 +0100

    parametros

---
+++ anotações.md
@@ -0,0 +1,5 @@
+# Hi
+
+Parametro é um valor dinamico que o usuario pode passar.
+
+Req :: receber dados de uma requisição.
--- servidor/servidorExpress.js
+++ servidor/servidorExpress.js
@@ -13,5 +13,11 @@ servidorExpress.get("/sobre", function (req, res) {
     res.end("Esse servidor foi construido com framework principal de node pra backend");
 });
 
+//Usando paramentros
+servidorExpress.get('/oi/:nome/:sobrenome', function(req, res){
+     res.end(req.params);
+});
+   
+
 //Melhor que essa linha esteja sempre no final mesmo.
 servidorExpress.listen(8081, function(){console.log("Express aberto com sucesso");});
